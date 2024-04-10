d2c4f474b74ce08fb959f110fdaaf1f89ee8fcab
George Baskerville <148500997+the-square-pixel@users.noreply.github.com>
2024-04-10 16:03:18 +0200

    fixed bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  integrations: [tailwind(), partytown()]
+  integrations: [tailwind()]
 });
