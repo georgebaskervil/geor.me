25d03cd472cb69770b92a012b78c1566a158aa08
George Baskerville <148500997+the-square-pixel@users.noreply.github.com>
2024-05-31 02:46:14 +0100

    get the imageboard working again with quickfixes

--- src/scripts/getImages.ts
+++ src/scripts/getImages.ts
@@ -5,7 +5,7 @@ function getImages() {
   const directoryPath = path.join(process.cwd(), 'public/image-assets');
   const files = fs.readdirSync(directoryPath);
   const images = files.filter(
-    (file) => file.endsWith('.avif') || file.endsWith('.webp'),
+    (file) => file.endsWith('.webp') || file.endsWith('.png'),
   );
   const baseNames = [
     ...new Set(
