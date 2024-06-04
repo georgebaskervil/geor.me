c73945e93f9c9020eec58e0148ddd3921bebbcd2
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2024-06-04 03:26:45 +0100

    tweaks and changes

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: 'https://georgebaskerville.me',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react()],
 });
